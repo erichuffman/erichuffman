d5c3705a726b932473988b23252d816dffae661b
Eric Huffman <eric@erichuffman.com>
2022-02-15 21:34:53 -0800

    Adds pause button, fixes nav empty focus, adds focus styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('normalize.css/normalize.css');
 require('@fortawesome/fontawesome-free/scss/fontawesome.scss');
 require('@fortawesome/fontawesome-free/scss/brands.scss');
+require('@fortawesome/fontawesome-free/scss/solid.scss');
 require('./styles/styles.scss');
 
 document.addEventListener("DOMContentLoaded", () => {
